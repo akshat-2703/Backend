cad0e7686921ebd80d36249c122b3c3aee0d61c5
akshat-2703 <akshatbhatt40@gmail.com>
2024-08-15 13:16:31 +0530

    n

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-
+/*
 const fs = require('fs');
 //require fs means aapne fs module o bhar nikala h node s aur usko use kr liya
-/*
+
 fs.writeFile("hey.txt","hey hello kaise ho",function(err){
     if(err)
      console.log(err) ;
@@ -41,5 +41,13 @@ fs.rmdir("./copy",{recursive: true},function(err){
     if(err) console.error(err) ;
     else console.log("removed");
 })
-    */
-   
+    
+   const http = require('http') ;
+
+   const server = http.createServer(function(req,res){
+    res.end("hello world") ;
+   })
+   server.listen(3000) ;
+   */
+  console.log("hello wordl");
+  
