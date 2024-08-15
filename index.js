ae61c2b211d6c848d459dca02cb5bd849d36719e
akshat-2703 <akshatbhatt40@gmail.com>
2024-08-15 19:10:38 +0530

    fi

--- index.js
+++ index.js
@@ -48,6 +48,43 @@ fs.rmdir("./copy",{recursive: true},function(err){
     res.end("hello world") ;
    })
    server.listen(3000) ;
-   */
+   
   console.log("hello wordl");
+  */
+  const express = require('express')
+  //as jo bhi cheeze is 'express' m wo sb nikl k express const m aa chuki h 
+  const app =   express() ;
+  //as basically jo bhi data express js ka h wo sb y app kr skta h 
+  //routes create krna 
+  //requestHandler ek middleware hota hai 
+  /* //basically understand of middleware
+ app.use(function(req,res,next){
+    console.log('middleware chala');
+    next() ;
+ });
+ app.use(function(req,res,next){
+    console.log("ek aur middleware chala") ;
+    next() ;
+ })*/
+ //form handling session and cookie 
   
+ app.use(express.json()) ;
+ app.use(express.urlencoded({extended : true})) ;
+
+  app.get("/",function(req,res){
+    res.send("champion meeeera anuuuj") ;
+  })
+  app.get("/about",function(req,res){
+    res.send("about page hai y ") ;
+  })
+  
+  app.get("/profile",function(req,res,next){
+ res.send("done") //y jayga console p 
+  })
+  /*
+  app.use((err,req,res,next) => {
+    console.log(err.stack);
+   res.status(500).send('Something went wrong,we dont have any idea') ; //y jayga frontend p 
+  })*/
+
+  app.listen(3000) ;
